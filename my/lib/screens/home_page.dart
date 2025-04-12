4a7a2d166afce33d4ab68ec23e5920a0d980c74c
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-12 21:03:13 +0300

    Update AndroidManifest.xml permissions and enhance weather display layout in HomePage

--- my/lib/screens/home_page.dart
+++ my/lib/screens/home_page.dart
@@ -30,38 +30,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: ListView.builder(
-      itemCount: _weather.length,
-      itemBuilder: (context, index) {
-        final WeatherModel weather = _weather[index];
-        return Container(
-          padding: const EdgeInsets.all(20),
-          margin: const EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(children: [
-            Image.network(weather.ikon,
-                width: 100,
-                height: 100,
-                fit: BoxFit.cover),
-                Padding(padding: EdgeInsets.only(top:15,bottom:25),
-                child: Text(
-                  "${weather.gun}\n ${weather.durum.toUpperCase()} ${weather.derece}°",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+      body: Center(
+        child: ListView.builder(
+          itemCount: _weather.length,
+          itemBuilder: (context, index) {
+            final WeatherModel weather = _weather[index];
+            return Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Image.network(weather.ikon,
+                      width: 100, height: 100, fit: BoxFit.cover),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15, bottom: 25),
+                    child: Text(
+                      "${weather.gun}\n ${weather.durum.toUpperCase()} ${weather.derece}°",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-
-                ])
-          ],)
-        );
-      },
-    )));
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Gündüz: ${weather.derece}°",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                          Text("Gece: ${weather.gece}°",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
